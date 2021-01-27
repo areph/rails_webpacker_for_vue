a245cf6f8249b1bef551f0041ada6a700a6385ef
areph <40balmung@gmail.com>
2021-01-27 14:48:25 +0900

    change root page to the top of vue.js

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'top/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'top#top'
+
+  get '*path', to: 'top#top', format: false
 end
